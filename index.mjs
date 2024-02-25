8c405e30eb214fd458fbd946b4620182b2244f23
Emile Nijssen <emile@emilenijssen.nl>
2024-02-25 19:49:13 +0100

    wip

--- index.mjs
+++ index.mjs
@@ -1,3 +1,7 @@
 export { default as LuxioDevice } from './lib/LuxioDevice.mjs';
 export { default as LuxioDiscovery } from './lib/LuxioDiscovery.mjs';
 export { default as LuxioUtil } from './lib/LuxioUtil.mjs';
+
+export const NUPNP_URL = 'https://nupnp.luxio.lighting';
+export const AP_ADDRESS = '192.168.4.1';
+export const AP_URL = `http://${AP_ADDRESS}`;
