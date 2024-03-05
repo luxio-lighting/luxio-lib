83a360566402ccc4dafeb9f97cda4d125db779d1
Emile Nijssen <emile@emilenijssen.nl>
2024-03-05 15:31:16 +0100

    wip

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export { default as LuxioDevice } from './lib/LuxioDevice.mjs';
 export { default as LuxioDiscovery } from './lib/LuxioDiscovery.mjs';
+export { default as LuxioDiscoveryStrategy } from './lib/LuxioDiscoveryStrategy.mjs';
+export { default as LuxioDiscoveryStrategyAP } from './lib/LuxioDiscoveryStrategyAP.mjs';
+export { default as LuxioDiscoveryStrategyMDNS } from './lib/LuxioDiscoveryStrategyMDNS.mjs';
+export { default as LuxioDiscoveryStrategyNUPNP } from './lib/LuxioDiscoveryStrategyNUPNP.mjs';
 export { default as LuxioUtil } from './lib/LuxioUtil.mjs';
-
-export const NUPNP_URL = 'https://nupnp.luxio.lighting';
-export const AP_ADDRESS = '192.168.4.1';
-export const AP_URL = `http://${AP_ADDRESS}`;
